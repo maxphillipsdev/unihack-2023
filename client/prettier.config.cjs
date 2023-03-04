2c779ba5f8bfe3241a2a673fdc6a7489abc8ba21
Max Phillips <max@maxphillips.dev>
2023-03-04 14:20:40 +1100

    Dev/setup-repo (#1)

---
+++ client/prettier.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require("prettier-plugin-organize-imports"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+};
---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-300">
+      <p>jdsjklfnadskjndfsi</p>
+    </div>
+  );
+}
+
+export default App;

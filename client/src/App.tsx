b5b180cf5904917a2f568695505f1ef0ed0cc657
Max Phillips <max@maxphillips.dev>
2023-03-04 15:48:02 +1100

    setup aliases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <div className="bg-blue-300">
+    <div
+      className="flex h-full justify-center
+      bg-gray-100 text-4xl
+    text-gray-900"
+    >
       <p>jdsjklfnadskjndfsi</p>
     </div>
   );

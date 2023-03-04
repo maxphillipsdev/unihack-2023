c72a46818b446f5bfb88234d1a5d40c0502377c9
Max Phillips <max@maxphillips.dev>
2023-03-04 17:24:56 +1100

    basic prisma setup

--- api/chat.ts
+++ api/chat.ts
@@ -1,4 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export default function handler(
   request: VercelRequest,
